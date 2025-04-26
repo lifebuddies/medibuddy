32689995cc6aa8dd589909a6cdea779aa9ea7019
IBruteDude <kamarf505@gmail.com>
2025-04-26 14:27:28 +0300

    fix(db): configured the medibuddy schema correctly

--- backend/identity/src/main/java/com/medibuddy/identity/IdentityApplication.java
+++ backend/identity/src/main/java/com/medibuddy/identity/IdentityApplication.java
@@ -2,8 +2,10 @@ package com.medibuddy.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IdentityApplication {
 
 	public static void main(String[] args) {
